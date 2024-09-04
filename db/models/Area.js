0b5c44c867ec21da50266bea2d6c91746c829d27
sgoithw <birch_deck_0s@icloud.com>
2024-09-04 22:33:47 +0200

    Changed table name according to project requirements

--- db/models/Area.js
+++ db/models/Area.js
@@ -2,7 +2,7 @@ import { DataTypes } from "sequelize";
 import sequelize from "../sequelize.js";
 
 const Area = sequelize.define(
-  "areas",
+  "Areas",
   {
     name: {
       type: DataTypes.STRING,
