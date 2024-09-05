ae0c6d46ee7c71aa3fe4763729fed6622a1ef8a3
999Ralex999 <140698368+999Ralex999@users.noreply.github.com>
2024-09-05 17:35:08 +0300

    done please check

--- app.js
+++ app.js
@@ -1,48 +1,50 @@
-import 'dotenv/config';
-import express from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
+import "dotenv/config";
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
 
 import sequelize from "./db/sequelize.js";
-import categoriesRouter from './routes/categoriesRouter.js';
-import ingredientsRouter from './routes/ingredientsRouter.js';
+import categoriesRouter from "./routes/categoriesRouter.js";
+import ingredientsRouter from "./routes/ingredientsRouter.js";
 import areasRouter from "./routes/areasRouter.js";
+import testimonialsRouter from "./routes/testimonials.js";
 
 const WEB_SERVER_PORT = Number(process.env.PORT) || 3000;
 
 const app = express();
 
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/status', (_, res) => {
-  res.json({ status: 'OK' });
+app.use("/api/status", (_, res) => {
+  res.json({ status: "OK" });
 });
 
-app.use('/api/categories', categoriesRouter);
-app.use('/api/ingredients', ingredientsRouter);
+app.use("/api/categories", categoriesRouter);
+app.use("/api/ingredients", ingredientsRouter);
 app.use("/api/areas", areasRouter);
+app.use("/api/testimonials", testimonialsRouter);
 
 app.use((_, res) => {
-  res.status(404).json({ message: 'Route not found' });
+  res.status(404).json({ message: "Route not found" });
 });
 
 app.use((err, req, res, next) => {
-  const { status = 500, message = 'Server error' } = err;
+  const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
 });
 
 try {
   console.log(
-    'Application started. Establishing connection to the database...'
+    "Application started. Establishing connection to the database..."
   );
   await sequelize.authenticate();
-  console.log('Database connection successful');
+  console.log("Database connection successful");
   app.listen(WEB_SERVER_PORT, () => {
     console.log(`Server is running. Use our API on port: ${WEB_SERVER_PORT}`);
   });
 } catch (error) {
-  console.log('Unable to connect to the database:', error.message);
+  console.log("Unable to connect to the database:", error.message);
   process.exit(1);
 }
---
+++ db/models/Testimonial.js
@@ -0,0 +1,11 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../sequelize.js";
+
+const Testimonial = sequelize.define("Testimonial", {
+  testimonial: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+export default Testimonial;
