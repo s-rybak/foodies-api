457b7cfc8fad6abc557d154e5c9b529b9091cd6c
Oleksandr Romashko <alex.rmshk@gmail.com>
2024-09-06 15:55:50 +0200

    Update User avatar default to be false

--- db/models/User.js
+++ db/models/User.js
@@ -56,6 +56,10 @@ const User = sequelize.define(
     },
     avatar: {
       type: DataTypes.STRING,
+      defaultValue: null,
+      validate: {
+        notEmpty: true,
+      },
     },
   },
   {
