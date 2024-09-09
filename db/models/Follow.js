50409f5e844fb5bcca060c5ce3318050b3cfc0a1
OlhaOs <olalaadka@gmail.com>
2024-09-09 12:21:34 +0300

    table follow

--- db/models/Follow.js
+++ db/models/Follow.js
@@ -1,26 +1,15 @@
-import { DataTypes } from 'sequelize';
-import sequelize from '../sequelize.js';
+import { DataTypes } from "sequelize";
+import sequelize from "../sequelize.js";
 
-const Follow = sequelize.define(
-  'follow',
-  {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
-    },
-    followerId: {
-      type: DataTypes.UUID,
-      allowNull: false,
-    },
-    followedId: {
-      type: DataTypes.UUID,
-      allowNull: false,
-    },
+const Follow = sequelize.define("follow", {
+  followerId: {
+    type: DataTypes.UUID,
+    allowNull: false,
   },
-  {
-    timestamps: true,
-  }
-);
+  followingId: {
+    type: DataTypes.UUID,
+    allowNull: false,
+  },
+});
 
 export default Follow;
